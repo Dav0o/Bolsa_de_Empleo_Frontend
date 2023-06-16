1999a2f9e8092b6edb640ff02b7e05b9dff3f72e
javie <javierstdiaz@gmail.com>
2023-06-15 23:56:02 -0600

    post de candidatoOferta funcional

--- src/pages/offers/components/Postulate.jsx
+++ src/pages/offers/components/Postulate.jsx
@@ -4,52 +4,38 @@ import { useQuery } from 'react-query';
 
 function Postulate({ ofertaDescripcion, ofertaId }) {
   const { data, isLoading, isError } = useQuery("candidatos", getCandidatos, { enable: true });
-  const [successMessage, setSuccessMessage] = useState('');
-  const [postulate, setPostulate] = useState(
-    {
-      "candidatoId": null,
-      "ofertaId": null
-    }
-  );
-
-  const handleSubmit = async (event) => {
-     event.preventDefault();
-    try {
-      setIsLoading(true);
-      const response = await fetch('https://localhost:7209/api/CandidatoOfertas', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(postulate),
-      });
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      // Reset form fields after successful submission
-      setPostulate({
-        "candidatoId": null,
-        "ofertaId": null
-      });
-
-      setSuccessMessage('¡Registrado éxitosamente!');
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   const [email, setEmail] = useState('');
 
-const handlePostular = () => {
-      
-      const matchingCandidate = data.find(candidate => candidate.correo_Electronico === email);
+  const handlePostular = async () => {
+
+    const matchingCandidate = data.find(candidate => candidate.correo_Electronico === email);
       
     if (matchingCandidate) {
-        console.log('Correo encontrado:', matchingCandidate.correo_Electronico);
-        console.log('ID:', matchingCandidate.id)
-        console.log('IdOferta', ofertaId)
+      const postPostulate = {
+        candidatoId: matchingCandidate.id,
+        ofertaId: ofertaId
+      };
+
+      try {
+        const response = await fetch('https://localhost:7209/api/CandidatoOfertas', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(postPostulate)
+        });
+      
+        if (response.ok) {
+          console.log('postulacion completa')
+
+        }
+        else {
+          console.log('postulacion error')
+        }
+      } catch (error) {
+        console.log('erroor post:', error);
+      }
     } else {
       console.log('Correo no encontrado');
     }
