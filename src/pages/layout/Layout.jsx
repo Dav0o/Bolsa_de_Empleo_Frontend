8d30ffb1dc815d47788915cd1f596eef58d8dda4
David <davidacumora@gmail.com>
2023-06-24 13:30:37 -0600

    Actualizar el nav

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,30 +1,48 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Outlet, NavLink } from "react-router-dom";
-import Candidate from "../candidate/Candidate";
+import { useState } from "react";
 import '../../stylesheets/Nav.css'
 
 const Layout = () => {
+
+  const [activeLink, setActiveLink] = useState('');
+
+  const handleNavLinkClick = (link) => {
+    setActiveLink(link);
+  }
   const queryClient = new QueryClient();
   return (
     <>
       <nav className="nav">
-        <NavLink className="" to="/">
-          Home
-        </NavLink>
-        <NavLink className="" to="/add-candidate">
-          {" "}
-          AddCandidates
-        </NavLink>
-        <NavLink className="" to="/candidates">
-          {" "}
-          Candidates
-        </NavLink>
-        <NavLink className="" to="/offers">
-          {" "}
-          Offers
-        </NavLink>
-        
-      </nav>
+      <NavLink
+        className={`home ${activeLink === 'home' ? 'active' : ''}`}
+        to="/"
+        onClick={() => handleNavLinkClick('home')}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={`add ${activeLink === 'add' ? 'active' : ''}`}
+        to="/add-candidate"
+        onClick={() => handleNavLinkClick('add')}
+      >
+        Add Candidates
+      </NavLink>
+      <NavLink
+        className={`candidate ${activeLink === 'candidate' ? 'active' : ''}`}
+        to="/candidates"
+        onClick={() => handleNavLinkClick('candidate')}
+      >
+        Candidates
+      </NavLink>
+      <NavLink
+        className={`oferta ${activeLink === 'oferta' ? 'active' : ''}`}
+        to="/offers"
+        onClick={() => handleNavLinkClick('oferta')}
+      >
+        Offers
+      </NavLink>
+    </nav>
 
       <main>
         <QueryClientProvider client={queryClient}>
@@ -35,4 +53,4 @@ const Layout = () => {
   );
 };
 
-export default Layout;
+export default Layout;
